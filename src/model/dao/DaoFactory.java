a71dd388ce503a2b55c9df54ae0e885c58cf46f3
Lucas Soares <polonio230@hotmail.com>
2024-06-07 11:21:10 -0300

    doc(DaoFactory): add documentation using javadoc

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,32 @@
 package model.dao;
 
-import model.dao.impl.SellerDaoJDBC;
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+
+/**
+ * Classe que segue o padrão Factory Method e tem o propósito de criar 
+ * instâncias de Data Access Objects (DAOs) específicos
+ * @author polon
+ *
+ */
 public class DaoFactory {
 	
-	// Retorna o tipo da interface mas internamente instancia uma implementação
-	// A estratégia é não expor a implementação
+	/**
+	 * Instancia um objeto do tipo SellerDaoJDBC, implementação de SellerDao
+	 * @return a instância do tipo implementação de sellerDao
+	 */
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
+	
+	/**
+	 * Instancia um objeto do tipo DeparmentDaoJDBC, implementação de DeparmentDao 
+	 * @return a instância do tipo implementação de DepartmentDao
+	 */
+	public static DepartmentDao createDeparmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+	
 }
